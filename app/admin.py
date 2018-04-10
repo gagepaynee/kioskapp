5b9bfd6e671f305efc5ad485e0a92f14365d034d
gagepaynee <35413671+gagepaynee@users.noreply.github.com>
2018-04-10 15:50:36 -0400

    Trying to fix file path problem

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Competition, Club, Major, OurStory
+from /app/models.py import Competition, Club, Major, OurStory
 
 # Register your models here.
 
